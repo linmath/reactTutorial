f51113a6ed6d20d62662031aca28ad9d301b9e05
linmath <alina@erl-a.de>
2024-04-19 14:04:57 +0200

    new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Info from "./Info.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Info />
+      <AddItem />
     </div>
   );
 }
+
+function AddItem() {
+  const value = "default";
+  return(
+    <form>
+      <label for="text-form">Type something: </label>
+      <input type ="text" value={value} id="text-form"/>
+    </form>
+  );
+}
+
 
 export default App;
